f1f7e16c838765c050da3dea0256470795db4a32
clesioosantos <clesio0420@gmail.com>
2024-07-15 00:03:31 -0300

    edit all main tf 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,18 +119,3 @@ resource "aws_ecs_service" "flask_service" {
     container_port   = 5000
   }
 }
-
-variable "docker_image" {
-  description = "The Docker image for the Flask app"
-  type        = string
-}
-
-variable "subnets" {
-  description = "The subnets for the ECS service"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "The VPC ID for the ECS service"
-  type        = string
-}
